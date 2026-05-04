6c3b57f8dcdf4f8302ab20e2a679e2e322dbe8d2
lina <a88091500@gmail.com>
2026-05-04 10:26:14 +0800

    子字串與字串綜合應用

---
+++ 1150504/02-datetime.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>日期/時間處理</title>
+</head>
+
+<body>
+     <h2>給定兩個日期，計算中間間隔天數</h2>
+     <ul>
+          <li>起始時間:2026-04-19</li>
+          <li>結束時間:2026-05-04</li>
+     </ul>
+
+     <?php
+
+
+     ?>
+</body>
+
+</html>
